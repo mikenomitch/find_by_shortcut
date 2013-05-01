0445e56c49b0aadd7ff11678ec43a8175e67c0bd
Mike Nomitch <mikenomitch@gmail.com>
2013-04-30 23:23:52 -0500

    modifies activerecord base rather than class now

--- fbstring.rb
+++ fbstring.rb
@@ -1,31 +1,34 @@
-# If you want to add code to individual classes, use something like this
-
-# class ClassName
-#   def self.method_missing(method, *args)
-#     if method.to_s.starts_with?("fb")
-#       remainder = method.to_s[2..-1]
-#       attribute = User.new.attributes.collect{|e| e[0]}.select{|e| e.starts_with?(remainder)}.first
-#       new_method = "find_by_" + attribute
-#       send(new_method.to_s, args)
-#     else
-#       super
-#     end
-#   end
-# end
-
-class Class
-  def method_missing(method, *args)
+class ActiveRecord::Base
+  def self.method_missing(method, *args)
     if method.to_s.starts_with?("fb")
       remainder = method.to_s[2..-1]
-      attribute = self.new.attributes.collect{|e| e[0]}.select{|e| e.starts_with?(remainder)}.first
+      attribute = self.new.attributes.keys.select{|e| e.starts_with?(remainder)}.first
       if attribute == nil
-        super
+        super *args
       else
         new_method = "find_by_" + attribute
         send(new_method.to_s, args)
       end
     else
-      super
+      super *args
     end
   end
 end
+
+
+# class Class
+#   def method_missing(method, *args)
+#     if method.to_s.starts_with?("fb")
+#       remainder = method.to_s[2..-1]
+#       attribute = self.new.attributes.keys.select{|e| e.starts_with?(remainder)}.first
+#       if attribute == nil
+#         super *args
+#       else
+#         new_method = "find_by_" + attribute
+#         send(new_method.to_s, args)
+#       end
+#     else
+#       super *args
+#     end
+#   end
+# end
